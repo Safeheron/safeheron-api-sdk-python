2b8466422b9562f6350b381c0d07201b9475b16e
absorprofess <absorprofess@outlook.com>
2025-04-07 14:19:57 +0800

    add cosigner v3 demo

--- demo/api_demo/co_signer/co_signer_converter_demo.py
+++ demo/api_demo/co_signer/co_signer_converter_demo.py
@@ -16,13 +16,15 @@ class TestCoSigner:
         config = read_yaml("./config.yaml")
         converter = CoSignerConverter(config)
         # The CoSignerCallBack received by the controller
+        # Visit the following link to view the request data specification：https://docs.safeheron.com/api/en.html#API%20Co-Signer%20Request%20Data
         co_signer_call_back = {}
-        biz_content = converter.request_convert(co_signer_call_back)
+        biz_content = converter.request_v3_convert(co_signer_call_back)
         # According to different types of CoSignerCallBack, the customer handles the corresponding type of business logic.
         print(biz_content)
 
-        coSignerResponse = CoSignerResponse()
-        coSignerResponse.approve = True
-        coSignerResponse.txKey = "TxKey that needs to be approved"
-        encryptResponse = converter.response_converter_with_new_crypto_type(coSignerResponse)
+        # Visit the following link to view the response data specification.：https://docs.safeheron.com/api/en.html#Approval%20Callback%20Service%20Response%20Data
+        coSignerResponse = CoSignerResponseV3()
+        coSignerResponse.action = "<Replace with APPROVE or REJECT>"
+        coSignerResponse.approvalId = "<Replace with the approvalId data from the request>"
+        encryptResponse = converter.response_v3_converter(coSignerResponse)
         # The customer returns encryptResponse after processing the business logic.
